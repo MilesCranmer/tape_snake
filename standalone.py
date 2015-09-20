8c0249727a4c27511168d1ca5e16d45459df8d6b
mdtmc <miles.cranmer@gmail.com>
2015-09-20 16:27:23 -0400

    The head moves!

--- standalone.py
+++ standalone.py
@@ -1,11 +1,11 @@
 #This program allows you to play a game of snake on 
 #BlinkyTape
 import numpy as np
-
 #matplotlib shows an alternative
 #view of the game baord
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import time
 
 #dimensions of our blinky surface
 leds = 25
@@ -13,10 +13,19 @@ tapes = 7
 
 move = {'right':np.array([1,0]),'left':np.array([-1,0]),
 			'up':np.array([0,1]),'down':np.array([0,-1])}
+
+
+def checkBounds(point):
+	if point[0] < 0 or point[0] - leds  <= 0:
+		return False
+	if point[1] < 0 or point[1] - tapes <= 0:
+		return False
+	return True
+
+head = np.array([int(leds/2.0),int(tapes/2.0)])
 #the snake is an array of points, 
 #with the head listed first:
-start = np.array([int(leds/2.0),int(tapes/2.0)])
-snake = [start,start+move['left'],start+2*move['left']]
+snake = [head,head+move['left'],head+2*move['left']]
 #current direction of the snake
 direc = 'right'
 
@@ -24,4 +33,15 @@ board = np.zeros((7,25))
 for point in snake:
 	board[point[1],point[0]] = 1
 plt.imshow(board, cmap = cm.Greys_r, interpolation="none")
+plt.ion()
 plt.show()
+for i in range(2):
+	head += move[direc]
+	print head
+	board = np.zeros((7,25))
+	for point in snake:
+		board[point[1],point[0]] = 1
+	plt.imshow(board, cmap = cm.Greys_r, interpolation="none")
+	plt.draw()
+	time.sleep(0.5)
+

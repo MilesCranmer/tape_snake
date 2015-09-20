cf58ad23e2f1927dac581bbe736ad15f2bb08ba8
mdtmc <miles.cranmer@gmail.com>
2015-09-20 15:39:00 -0400

    treating all points as np arrays

--- standalone.py
+++ standalone.py
@@ -7,6 +7,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-board = np.zeros((25,7))
+#dimensions of our blinky surface
+leds = 25
+tapes = 7
+
+move = {'right':np.array([1,0]),'left':np.array([-1,0]),
+			'up':np.array([0,1]),'down':np.array([0,-1])}
+#the snake is an array of points, 
+#with the head listed first:
+start = np.array([int(leds/2.0),int(tapes/2.0)])
+snake = [start,start+move['left'],start+2*move['left']]
+#current direction of the snake
+direc = 'right'
+
+board = np.zeros((7,25))
+for point in snake:
+	board[point[1],point[0]] = 1
 plt.imshow(board, cmap = cm.Greys_r, interpolation="none")
 plt.show()
